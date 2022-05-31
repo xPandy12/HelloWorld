d3d562fa2259d7fbc0acbc9de186ab04dd1511c2
MarcoSalazar <andressalazar714@gmail.com>
2022-05-31 17:51:20 -0500

    Message

--- app/src/main/java/com/marcosalazar/helloworld/MainActivity.kt
+++ app/src/main/java/com/marcosalazar/helloworld/MainActivity.kt
@@ -2,11 +2,14 @@ package com.marcosalazar.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"Hola mundo",Toast.LENGTH_LONG)
+            .show()
     }
 }
