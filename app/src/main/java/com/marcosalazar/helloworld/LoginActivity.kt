d5019ab6594b27dbb0d63ad814ac28542906b02b
Veronica <veritoasa@gmail.com>
2022-06-01 16:52:44 -0500

    Implementación MockUp AndroidStudio

---
+++ app/src/main/java/com/marcosalazar/helloworld/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.marcosalazar.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
